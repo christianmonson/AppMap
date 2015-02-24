7efadce0512cd02b9cbdba31ff7080d17c52e684
Christian Monson <christianmonson@Christians-MacBook-Pro.local>
2015-02-24 14:54:44 -0700

    sets up view controllers and completes view controller 1
    push to more info
    
    Signed-off-by: Christian Monson <christianmonson@Christians-MacBook-Pro.local>

---
+++ AppMap/MoreInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreInfoViewController.h
+//  AppMap
+//
+//  Created by Christian Monson on 2/24/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreInfoViewController : UIViewController
+
+@end
